6e93ebefe0a518e47f3b03c911ecc4162e32d061
latif83 <latifm8360@gmail.com>
2025-12-05 15:35:23 +0000

    Updates!

--- tryimage.php
+++ tryimage.php
@@ -46,7 +46,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_FILES["image"])) {
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en"> 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
